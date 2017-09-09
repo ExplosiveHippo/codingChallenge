115458f46019ab7d99fbbb83dc1e3a21fdd0f1e1
Lance Laughlin <lancealaughlin@gmail.com>
2017-09-09 16:14:17 -0400

    Showing selected content :)

--- src/components/main_section.js
+++ src/components/main_section.js
@@ -1,10 +1,49 @@
 import React, { Component } from 'react';
 
 class MainSection extends Component{
+
+	renderAppKeys(app_keys){
+		return app_keys.map(item =>{
+			console.log(item);
+			return(
+				<li>{item}</li>
+			)
+		});
+	}
+
+	renderContentArea() {
+		let itemsShown = this.props.content;
+		return itemsShown.map(item => {
+			console.log(item);
+			return (
+				<div key={item.id} className="detailItem">
+					<h2>{item.name}</h2>
+					<p>Type: {item.data_type}</p>
+					<ul>
+						{this.renderAppKeys(item.app_keys)}
+					</ul>
+					<p>EverTrueFieldName: {item.name}</p>
+				</div>
+			)
+			
+		});
+	}
 	
 	render() {
-		console.log(this.props.content);
-		return(<div>Hi</div>);
+		if(this.props.content.length > 0){
+			return(
+				<div className="mainSection">
+					{this.renderContentArea()}
+				</div>
+			);
+		}else{
+			return(
+				<div className="mainSection">
+					<p>Select an section in the left menu to view all properties for that section</p>
+				</div>
+			)
+		}  
+		
 	}
 
 }
